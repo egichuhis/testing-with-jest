568a3a0be8b81d6f29aea7a0b3b182a894be9dd9
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-23 10:44:47 +0300

    Test for Reverse String

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-  function stringLength(string) {
-    const strLength = string.length;
+function stringLength(string) {
+  const strLength = string.length;
 
-    if (strLength === 0) {
-        throw new Error('String should have at least 1 character');
-    }
-    
-    if (strLength > 10) {
-        throw new Error('String should have a maximum of 10 characters');
-    }
-
-    return strLength;
+  if (strLength === 0) {
+    throw new Error('String should have at least 1 character');
   }
 
+  if (strLength > 10) {
+    throw new Error('String should have a maximum of 10 characters');
+  }
+
+  return strLength;
+}
+
+function reverseString(string) {
+  return string.split('').reverse().join('');
+}
+
+reverseString('Are');
 
 exports.stringLength = stringLength;
+exports.reverseString = reverseString;
--- methods.test.js
+++ methods.test.js
@@ -1,15 +1,20 @@
-const { stringLength } = require('./index');
+const { stringLength, reverseString } = require('./index.js');
 
 describe('Testing strings', () => {
-test('stringLength should throw an error for strings with less than 1 character', () => {
-  expect(() => {
-    stringLength('');
-  }).toThrow('String should have at least 1 character');
-});
+  test('stringLength should throw an error for strings with less than 1 character', () => {
+    expect(() => {
+      stringLength('');
+    }).toThrow('String should have at least 1 character');
+  });
 
-test('stringLength should throw an error for strings longer than 10 characters', () => {
-  expect(() => {
-    stringLength('This string is too long sir');
-  }).toThrow('String should have a maximum of 10 characters');
+  test('stringLength should throw an error for strings longer than 10 characters', () => {
+    expect(() => {
+      stringLength('This string is too long sir');
+    }).toThrow('String should have a maximum of 10 characters');
+  });
+
+  test('reverseString should return a reversed string', () => {
+    const result = reverseString('Omad');
+    expect(result).toBe('damO');
+  });
 });
- });
